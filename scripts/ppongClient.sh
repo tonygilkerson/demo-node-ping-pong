d4eae3c27224d50ba39d1f1e8c6f72719ec55b2e
tonygilkerson <tonygilkerson@yahoo.com>
2016-06-23 00:47:36 -0400

    adding v2

---
+++ scripts/ppongClient.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# $1 - loop this many times
+# $2 - url to hit
+# $3 - -p run tests in parallel 
+
+if [ "$3" == "-p" ]; then
+	printf "\nrunning tests in parallel..."
+	cat /dev/null > ./parallel-output.tmp
+else
+	printf "\nrunning tests in sequence..."
+fi
+
+for i in `seq 1 $1`; 
+do
+	if [ "$3" == "-p" ]; then
+		#curl -s $2 >> ./parallel-output.tmp &
+		curl -s $2 &
+	else
+		printf "\nseq [%s] date [%s]" "$i" `date +\"%Y-%m-%d_%H-%M-%S\"`
+		curl -s $2 
+	fi
+
+done
+wait
+
+if [ "$3" == "-p" ]; then
+	cat ./parallel-output.tmp
+fi
+
+printf "\nDone.";
+
