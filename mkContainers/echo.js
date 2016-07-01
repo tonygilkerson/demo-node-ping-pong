3ae0d073bf989c812012e55c21369827dc1a6cd1
tonygilkerson <tonygilkerson@yahoo.com>
2016-07-01 11:04:18 -0400

    minor changes

--- mkContainers/echo.js
+++ mkContainers/echo.js
@@ -8,12 +8,11 @@ const PORT=80;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-	
+
 	setTimeout(function() {
   		console.log('waist some time...');
-	
-		response.end('1 second ZZZzzz...Got request with path: [' + request.url + '] Responding from host: [' + hostname + '] -- v2');
-    		console.log("1 second ZZZzzz... Got request with path: [%s] Responding from host: [%s] -- v2",request.url,hostname);
+			response.end(' 1 second ZZZzzz...Got request with path: [' + request.url + '] Responding from host: [' + hostname + '] ');
+    	console.log(" 1 second ZZZzzz... Got request with path: [%s] Responding from host: [%s] ",request.url,hostname);
 	}, 1000);
 
 }
@@ -24,5 +23,5 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("v2 Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://localhost:%s", PORT);
 });
